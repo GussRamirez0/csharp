59f14336fae8d5dd575e76eb1a12f073735d9d16
Luis Ramírez <130495704+GussRamirez0@users.noreply.github.com>
2024-02-23 15:52:01 -0600

    Primer Commit

---
+++ primerprograma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class MiPrimerPrograma
+    {
+        public static void Main(string[] args)
+        {
+            ConsoleWriteLine("Hazlo facil, haz Click!");
+        }
+    }
+}
